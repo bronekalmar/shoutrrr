be77d0024684835163407fae9e083d917e35ead3
Вадим Подопросветов <v.podoprosvetov@datamart.ru>
2023-07-17 15:04:56 +0300

    v.0.7.3

--- internal/meta/version.go
+++ internal/meta/version.go
@@ -1,7 +1,7 @@
 package meta
 
 // Version of Shoutrrr
-const Version = `0.7.2`
+const Version = `0.7.3`
 
 // DocsVersion is prepended to documentation URLs and usually equals MAJOR.MINOR of Version
 const DocsVersion = `v0.7`
